a81efb7e8d5eb2178ec16e3aa780ef976312c599
Lukas Vyhlidka <Lucky.V@gmail.com>
2011-09-21 15:58:50 +0200

    App skeleton

---
+++ LightsSeq/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Lucky
+ *
+ * Created on 21. září 2011, 15:36
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World";
+    return 0;
+}
+
